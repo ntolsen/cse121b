383aed62006f0309c58ddb17f4a48b7a52ed1962
ntolsen <nicktolsen13@gmail.com>
2023-09-23 21:21:35 -0600

    commit

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -6,25 +6,22 @@
 let fullName = 'Nicholas Olsen';
 let currentYear = '2023';
 let profilePicture = 'images/me.jpg';
-
-
 /* Step 3 - Element Variables */
-
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
 /* Step 4 - Adding Content */
-
-
-
-
-
-
+nameElement.innterHTML = `<strong>${fullName}<strong>`;
+let textContent = `${currentYear}`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile Picture of ${fullName}`);
 /* Step 5 - Array */
-
-
-
-
-
-
+favFoods = ['Burgers', 'Steak', 'Burritos', 'Pizza', 'Schnitzel']
+foodElement.innerHTML += `<br>${favFoods}`;
+favFood = 'Fettucine Alfredo';
+favFoods.push(favfood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods}`;

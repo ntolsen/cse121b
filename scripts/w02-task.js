32b045ee6deec9f14607d3095716a5497634f6f4
ntolsen <nicktolsen13@gmail.com>
2023-09-23 21:38:38 -0600

    commit

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -10,9 +10,10 @@ let profilePicture = 'images/me.jpg';
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('image')
 /* Step 4 - Adding Content */
-nameElement.innterHTML = `<strong>${fullName}<strong>`;
-let textContent = `${currentYear}`;
+nameElement.innerHTML = `<strong>${fullName}<strong>`;
+let year = document.getElementById("#year").textContent;
 imageElement.setAttribute('src', profilePicture);
 imageElement.setAttribute('alt', `Profile Picture of ${fullName}`);
 /* Step 5 - Array */

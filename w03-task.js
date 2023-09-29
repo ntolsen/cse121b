837ca3cfe13efe9b53cdbe2e6e5e8dd12d6355e3
ntolsen <nicktolsen13@gmail.com>
2023-09-28 23:57:41 -0600

    commit

--- w03-task.js
+++ w03-task.js
@@ -2,41 +2,51 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-function add(number1, number2){
+function add(number1, number2) {
     return number1 + number2;
 }
 
-let addNumber1 = Number(document.querySelector('#add1').value);
-let addNumber2 = Number(document.querySelector('#add2').value);
-document.querySelector('#sum').value = add(addNumber1. addNumber2);
-document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+function addNumbers(add1, add2) {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+    document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+}
 /* Function Expression - Subtract Numbers */
 function subtract(number1, number2){
     return number1 - number2;
 }
 
-let subtractNumber1 = Number(document.querySelector('#subtract1').value);
-let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-document.querySelector('#difference').value = subtract(subtractNumber1. subtractNumber2);
-document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+function subtractNumbers(subtract1, subtract2) {
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+    document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
+}
+
 
 /* Arrow Function - Multiply Numbers */
 multiply((number1, number2) => number1 * number2);
 
-let multiplyNumber1 = Number(document.querySelector('#factor1').value);
-let multiplyNumber2 = Number(document.querySelector('#factor2').value);
-document.querySelector('#product').value = multiply(addNumber1. addNumber2);
-document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+function multiplyNumbers(factor1, factor2) {
+    let multiplyNumber1 = Number(document.querySelector('#factor1').value);
+    let multiplyNumber2 = Number(document.querySelector('#factor2').value);
+    document.querySelector('#product').value = multiply(addNumber1, addNumber2);
+    document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
+}
 
 /* Open Function Use - Divide Numbers */
 function divide(dividend, divisor){
     return dividend / divisor;
 }
 
-let divideNumber1 = Number(document.querySelector('#dividend').value);
-let divideNumber2 = Number(document.querySelector('#divisor').value);
-document.querySelector('#quotient').value = divide(divideNumber1. divideNumber2);
-document.querySelector('#divideNumbers').addEventListener('click', divide2Numbers);
+function divideNumbers(dividend, divisor) {
+    let divideNumber1 = Number(document.querySelector('#dividend').value);
+    let divideNumber2 = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(divideNumber1, divideNumber2);
+    document.querySelector('#divideNumbers').addEventListener('click', divide2Numbers);
+}
+
 
 /* Decision Structure */
 
